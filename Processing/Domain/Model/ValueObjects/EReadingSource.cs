9e202f01c23778bcc79603f70bcc432756a5c460
marckszz <mateocab100@gmail.com>
2026-07-11 12:50:06 -0500

    Update EReadingSource.cs

--- Processing/Domain/Model/ValueObjects/EReadingSource.cs
+++ Processing/Domain/Model/ValueObjects/EReadingSource.cs
@@ -1,3 +1,3 @@
 namespace Hampcoders.Electrolink.API.Processing.Domain.Model.ValueObjects;
 
-public enum EReadingSource { EdgeDevice, EdgeBufferReplay }
+public enum EReadingSource { EdgeDevice, EdgeBufferReplay, EDGE }
